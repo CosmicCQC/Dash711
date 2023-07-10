0050d8220880d80c6cf534658c8fe9a24766b2aa
CosmicCQC <139099140+CosmicCQC@users.noreply.github.com>
2023-07-10 01:52:36 -0400

    Update Py.py

--- Py.py
+++ Py.py
@@ -5,6 +5,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+from dash import dash_table
 import plotly.graph_objs as go
 
 # Initialize the app
